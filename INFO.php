0816d2b3bbf1206685db1dd103db71b86ab783a7
TRSONGIR <67713900+TRSONGIR@users.noreply.github.com>
2020-09-10 00:50:54 +0430

    master

---
+++ INFO.php
@@ -0,0 +1,13 @@
+<?php
+
+$tel="Geladiator_phishing ";
+
+$chanel="Geladiator_phishing ";
+
+$TOKENS="991466294:AAH2nu64U66vGU1gn7X30IjXJgkopbHG9PM";
+
+$IDS="-486409903";
+
+$Team="Geladiator";
+
+?>
